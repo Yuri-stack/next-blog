dcfbac84e1072e4b47114345444e05a6cdca8b3c
Yuri-stack <yurioliveirasilva1999@gmail.com>
2025-07-25 17:55:29 -0300

    feat.: Criando a funcionalidade de busca

--- src/templates/blog/components/post-card/post-card.tsx
+++ src/templates/blog/components/post-card/post-card.tsx
@@ -31,7 +31,7 @@ export function PostCard({ slug, title, description, image, date, author }: Post
                     </div>
                     <Image
                         src={image}
-                        alt=""
+                        alt={title}
                         width={288}
                         height={144}
                         className="w-full h-40 object-cover object-center rounded-t-[8px]"
@@ -39,7 +39,7 @@ export function PostCard({ slug, title, description, image, date, author }: Post
                 </div>
 
                 {/* Post info */}
-                <div className="px-2 mt-4 space-y-4">
+                <div className="px-2 my-4 space-y-4">
                     <h2 className="text-heading-sm text-gray-100 line-clamp-3">
                         {title}
                     </h2>
