b54c51c26f888699140480a5aa71dd542581a95b
Yuri-stack <yurioliveirasilva1999@gmail.com>
2025-08-21 20:18:53 -0300

    feat.: Entendendo SSG e ISR na App Router

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -6,6 +6,12 @@ type BlogPostPageProps = {
     params: Promise<{ slug: string }>   // esse 'slug' é devido o nome da pasta que esse arquivo está. Outro nome de pasta, outro nome de prop.
 }
 
+export const revalidate = 60;
+
+export async function generateStaticParams() {
+    return posts.map(post => ({ slug: post.slug }));
+}
+
 export default async function BlogPostPage({ params }: BlogPostPageProps) {
     const { slug } = await params;
     const post = posts.find((post) => post.slug === slug);
