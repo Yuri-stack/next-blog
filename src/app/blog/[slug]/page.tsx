a9a579423a30a5b915df7e408f01dd60c6f23bd7
Yuri-stack <yurioliveirasilva1999@gmail.com>
2025-08-21 20:05:17 -0300

    feat.: Criando a Página de Post

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,18 @@
+import { PostPage } from "@/templates/blog";
+import { notFound } from "next/navigation";
+import { posts } from "posts/posts";
+
+type BlogPostPageProps = {
+    params: Promise<{ slug: string }>   // esse 'slug' é devido o nome da pasta que esse arquivo está. Outro nome de pasta, outro nome de prop.
+}
+
+export default async function BlogPostPage({ params }: BlogPostPageProps) {
+    const { slug } = await params;
+    const post = posts.find((post) => post.slug === slug);
+
+    if (!post) notFound();  // redireciona ao 404 page
+
+    return (
+        <PostPage post={post} />
+    )
+}
