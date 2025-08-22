43507f8a660a0147e655bed7244327ddb2a3d454
Yuri-stack <yurioliveirasilva1999@gmail.com>
2025-08-22 14:44:42 -0300

    feat.: Melhorando o SEO da Página do Post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,9 +1,29 @@
 import { PostPage } from "@/templates/blog";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { posts } from "posts/posts";
 
 type BlogPostPageProps = {
     params: Promise<{ slug: string }>   // esse 'slug' é devido o nome da pasta que esse arquivo está. Outro nome de pasta, outro nome de prop.
+}
+
+export async function generateMetadata({ params }: BlogPostPageProps): Promise<Metadata> {
+    const { slug } = await params;
+    const post = posts.find((post) => post.slug === slug);
+
+    if (!post) {
+        return {};
+    }
+
+    return {
+        title: post.title,
+        description: post.description,
+        authors: [{ name: post.author.name }],
+        robots: 'index, follow',
+        openGraph: {
+            images: [post.image],
+        },
+    };
 }
 
 export const revalidate = 60;
