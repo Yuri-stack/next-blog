8adffd381c2afd588b2bf9f1736b5398bf0e721b
Yuri-stack <yurioliveirasilva1999@gmail.com>
2025-08-22 14:40:12 -0300

    feat.: Melhorando SEO na Página de Listagem de Posts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
 import { BlogList } from "@/templates/blog";
 import { posts } from "posts/posts";
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Blog',
+    description: 'Dicas e estratégias para impulsionar seu negócio',
+    robots: 'index, follow',
+    openGraph: {
+        title: 'Blog',
+        description: 'Dicas e estratégias para impulsionar seu negócio',
+        url: 'https://nextjs-fundamentos.vercel.app/og-image.jpg',
+        siteName: 'Site.Set',
+        locale: 'pt-BR',
+        type: 'website',
+        images: [
+            {
+                url: 'https://nextjs-fundamentos.vercel.app/og-image.jpg',
+                width: 800,
+                height: 600,
+                alt: 'Site.Set',
+            },
+        ],
+    },
+};
 
 // Esse componente é um React Server Component (RSC).
 // Tudo que estiver aqui, será executado do lado do Servidor
